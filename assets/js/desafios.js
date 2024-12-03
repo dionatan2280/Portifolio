79cf1a0ee82eaaeff0afdc090c94b57b1a38385e
dionatan2280 <dionatan2280@gmail.com>
2024-12-03 15:06:32 -0300

    Add files via upload

---
+++ assets/js/desafios.js
@@ -0,0 +1,15 @@
+function desafios(section) {
+  section.innerHTML = `<header>
+      <h4>Projetos</h4>
+      
+          <p>
+            Desenvolva uma página para ser seu portifólio.
+          </p>
+          <p>HTML - CSS - JS</p>
+        </div>
+      </div>
+      <a href="#projects" id="backToProjectsBtn" class="button">Voltar</a>
+    </div>`;
+}
+
+export { desafios };
